369584a203c46ca60bbe35dd1615fcb0119354b7
zas512 <zainalis.914@gmail.com>
2023-06-19 02:21:04 -0700

    2

--- models/dbModel.js
+++ models/dbModel.js
@@ -1,19 +1,20 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/dbConfig.js");
 
-const User = sequelize.define(
+// User model
+exports.User = sequelize.define(
   "User",
   {
-    user_id: {
+    USER_ID: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       allowNull: false,
     },
-    fName: {
+    USERNAME: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    lName: {
+    PASSWORD: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -23,4 +24,80 @@ const User = sequelize.define(
     timestamps: false,
   }
 );
-module.exports = User;
+
+// Employee model
+exports.Employee = sequelize.define(
+  "Employee",
+  {
+    EMP_ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    FIRST_NAME: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    LAST_NAME: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    TEAM_LEAD: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "employees",
+    timestamps: false,
+  }
+);
+
+// Project model
+exports.Project = sequelize.define(
+  "Project",
+  {
+    PROJECT_ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    NAME: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    DESCRIPTION: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ASSIGNED_BY: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "projects",
+    timestamps: false,
+  }
+);
+
+// Add associations
+exports.Employee.belongsToMany(exports.Project, {
+  through: "EmployeeProject",
+  foreignKey: "EMP_ID",
+  as: "AssignedProjects",
+});
+exports.Project.belongsToMany(exports.Employee, {
+  through: "EmployeeProject",
+  foreignKey: "PROJECT_ID",
+  as: "AssignedEmployees",
+});
+
+exports.Project.belongsTo(exports.User, {
+  foreignKey: "ASSIGNED_BY",
+  as: "AssignedByUser",
+});
+exports.User.hasMany(exports.Project, {
+  foreignKey: "ASSIGNED_BY",
+  as: "AssignedProjects",
+});
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const User = require("./models/dbModel");
+const sequelize = require("./config/dbConfig");
 
+//models (user,emp,proj)
+require("./models/dbModel");
+
+//server config
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(bodyParser.json());
 
-User.sync({ alter: true });
+//create, modify or sync tables
+sequelize.sync();
 
+//routes
 app.use("/", require("./routes/userRoutes"));
 
+//start server
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
