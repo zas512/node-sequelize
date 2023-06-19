f308294e96784fc742743aa9be9bc7c91e126867
zas512 <zainalis.914@gmail.com>
2023-06-18 22:09:24 -0700

    1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(bodyParser.json());
 
-User.sync({ alter: true });
+User.sync();
 
 app.get("/", (req, res) => {
   res.send("Hello");
